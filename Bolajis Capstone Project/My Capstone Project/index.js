45673050dbcebc440da03ce4ed25c87079e17b91
bolaji0001 <127899857+bolaji0001@users.noreply.github.com>
2024-01-22 12:04:14 +0100

    Add files via upload
    uploaded the files needed for the push request

---
+++ Bolajis Capstone Project/My Capstone Project/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+
+// app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req,res)=>{
+    res.render("index.ejs")
+})
+
+app.listen("3000", (req,res)=>{
+    console.log("App listening on port 3000")
+})
